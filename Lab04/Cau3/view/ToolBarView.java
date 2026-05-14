2186f433eec61bcecd76121b0ece14357f767e0c
khoitang-tech <24520868@gm.uit.edu.vn>
2026-05-14 14:14:20 +0700

    Bo sung code xu ly icon cho button

--- Lab04/Cau3/view/ToolBarView.java
+++ Lab04/Cau3/view/ToolBarView.java
@@ -1,6 +1,9 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JToolBar;
@@ -11,7 +14,7 @@ public class ToolBarView extends JFrame {
 	public ToolBarView() {
 
 		this.setTitle("Toolbar Example");
-		this.setSize(1200, 200);
+		this.setSize(900, 200);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new BorderLayout());
@@ -23,20 +26,61 @@ public class ToolBarView extends JFrame {
 		JToolBar toolBar = new JToolBar();
 
 		// Tạo các button theo yêu cầu
-		JButton btnOpen = new JButton("Open file");
-		JButton btnSave = new JButton("Save");
-		JButton btnSaveAs = new JButton("Save as");
-		JButton btnPrint = new JButton("Print");
-		JButton btnSendMail = new JButton("Send mail");
-		JButton btnFacebook = new JButton("Facebook");
-		JButton btnSlideShow = new JButton("Slide Show");
-		JButton btnHand = new JButton("Hand");
-		JButton btnSearch = new JButton("Search");
-		JButton btnPointer = new JButton("Pointer");
-		JButton btnHighlight = new JButton("Highlight");
-		JButton btnCamera = new JButton("Camera");
-		JButton btnRMS = new JButton("RMS");
-		JButton btnSign = new JButton("Sign");
+		JButton btnOpen = new JButton();
+		btnOpen.setActionCommand("Open file");
+		btnOpen.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_open.png"))));
+		
+		JButton btnSave = new JButton();
+		btnSave.setActionCommand("Save");
+		btnSave.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_save.png"))));
+		
+		JButton btnSaveAs = new JButton();
+		btnSaveAs.setActionCommand("Save as");
+		btnSaveAs.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_saveas.png"))));
+		
+		JButton btnPrint = new JButton();
+		btnPrint.setActionCommand("Print");
+		btnPrint.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_print.png"))));
+		
+		JButton btnSendMail = new JButton();
+		btnSendMail.setActionCommand("Send mail");
+		btnSendMail.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_mail.png"))));
+		
+		JButton btnFacebook = new JButton();
+		btnFacebook.setActionCommand("Facebook");
+		btnFacebook.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_facebook.png"))));
+		
+		JButton btnSlideShow = new JButton();
+		btnSlideShow.setActionCommand("Slide show");
+		btnSlideShow.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_slideshow.png"))));
+		
+		JButton btnHand = new JButton();
+		btnHand.setActionCommand("Hand");
+		btnHand.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_hand.png"))));
+		
+		JButton btnSearch = new JButton();
+		btnSearch.setActionCommand("Search");
+		btnSearch.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_search.png"))));
+		
+		JButton btnPointer = new JButton();
+		btnPointer.setActionCommand("Pointer");
+		btnPointer.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_pointer.png"))));
+		
+		JButton btnHighlight = new JButton();
+		btnHighlight.setActionCommand("Highlight");
+		btnHighlight.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_highlight.png"))));
+		
+		JButton btnCamera = new JButton();
+		btnCamera.setActionCommand("Camera");
+		btnCamera.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_camera.png"))));
+		
+		JButton btnRMS = new JButton();
+		btnRMS.setActionCommand("RMS");
+		btnRMS.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_rms.png"))));
+		
+		JButton btnSign = new JButton();
+		btnSign.setActionCommand("Sign");
+		btnSign.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().createImage(ToolBarView.class.getResource("icon_sign.png"))));
 
 		// Gắn ActionListener
 		btnOpen.addActionListener(controller);
